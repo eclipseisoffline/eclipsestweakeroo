95d3a4e9737de47baabce60ee0d3baa49d686c65
Eclipse <eclipse@eclipseisoffline.xyz>
2024-01-18 21:01:08 +0000

    tweakPlayerList mostly finished

--- src/main/java/xyz/eclipseisoffline/eclipsestweakeroo/mixin/PlayerListHudMixin.java
+++ src/main/java/xyz/eclipseisoffline/eclipsestweakeroo/mixin/PlayerListHudMixin.java
@@ -1,12 +1,19 @@
 package xyz.eclipseisoffline.eclipsestweakeroo.mixin;
 
+import java.util.Map;
 import net.minecraft.client.gui.hud.PlayerListHud;
 import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.scoreboard.Team;
 import net.minecraft.text.MutableText;
+import net.minecraft.text.PlainTextContent.Literal;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.GameMode;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -16,6 +23,18 @@ import xyz.eclipseisoffline.eclipsestweakeroo.config.AdditionalGenericConfig;
 
 @Mixin(PlayerListHud.class)
 public abstract class PlayerListHudMixin {
+    @Unique
+    private static final Map<GameMode, MutableText> GAMEMODE_TEXT = Map.of(
+            GameMode.SURVIVAL, MutableText.of(new Literal("S")).setStyle(Style.EMPTY.withColor(
+                    Formatting.RED)),
+            GameMode.CREATIVE, MutableText.of(new Literal("C")).setStyle(Style.EMPTY.withColor(
+                    Formatting.GREEN)),
+            GameMode.ADVENTURE, MutableText.of(new Literal("A")).setStyle(Style.EMPTY.withColor(
+                    Formatting.YELLOW)),
+            GameMode.SPECTATOR, MutableText.of(new Literal("SP")).setStyle(Style.EMPTY.withColor(
+                    Formatting.BLUE))
+    );
+
     @Shadow
     private Text header;
     @Shadow
@@ -23,15 +42,33 @@ public abstract class PlayerListHudMixin {
 
     @Inject(method = "getPlayerName", at = @At("HEAD"), cancellable = true)
     public void getPlayerName(PlayerListEntry playerListEntry, CallbackInfoReturnable<Text> callbackInfoReturnable) {
-        /*if (AdditionalDisableConfig.DISABLE_DISPLAY_NAME.getBooleanValue()) {
-            callbackInfoReturnable.setReturnValue(applyGameModeFormatting(playerListEntry,
-                    Team.decorateName(playerListEntry.getScoreboardTeam(),
-                            Text.literal(playerListEntry.getProfile().getName()))));
-        }*/
-        // todo
+        if (AdditionalFeatureToggle.TWEAK_PLAYER_LIST.getBooleanValue() && AdditionalGenericConfig.TWEAK_PLAYER_LIST_NAMES.getBooleanValue()) {
+            MutableText playerName = Team.decorateName(playerListEntry.getScoreboardTeam(), Text.literal(playerListEntry.getProfile().getName()));
+            playerName.append(Text.of(" - "));
+            playerName.append(GAMEMODE_TEXT.get(playerListEntry.getGameMode()));
+            playerName.append(Text.of(" - "));
+            playerName.append(MutableText.of(new Literal(playerListEntry.getLatency() + "ms"))
+                    .setStyle(getPingStyle(playerListEntry.getLatency())));
+            callbackInfoReturnable.setReturnValue(playerName);
+        }
     }
 
-    @Shadow protected abstract Text applyGameModeFormatting(PlayerListEntry entry, MutableText name);
+    @Unique
+    private static Style getPingStyle(int ping) {
+        if (ping <= 0) {
+            return Style.EMPTY.withColor(Formatting.DARK_GRAY);
+        } else if (ping <= 150) {
+            return Style.EMPTY.withColor(Formatting.GREEN);
+        } else if (ping <= 300) {
+            return Style.EMPTY.withColor(Formatting.YELLOW);
+        } else if (ping <= 600) {
+            return Style.EMPTY.withColor(Formatting.GOLD);
+        } else if (ping <= 1000) {
+            return Style.EMPTY.withColor(Formatting.RED);
+        }
+
+        return Style.EMPTY.withColor(Formatting.DARK_RED);
+    }
 
     @Redirect(method = "render",
             at = @At(value = "FIELD", opcode = Opcodes.GETFIELD, target = "Lnet/minecraft/client/gui/hud/PlayerListHud;header:Lnet/minecraft/text/Text;"))
@@ -50,5 +87,16 @@ public abstract class PlayerListHudMixin {
             return null;
         }
         return footer;
+    }
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/PlayerListEntry;getGameMode()Lnet/minecraft/world/GameMode;"))
+    private GameMode getGameMode(PlayerListEntry playerListEntry) {
+        if (AdditionalFeatureToggle.TWEAK_PLAYER_LIST.getBooleanValue()
+                && AdditionalGenericConfig.TWEAK_PLAYER_LIST_NAMES.getBooleanValue()
+                && playerListEntry.getGameMode() == GameMode.SPECTATOR) {
+            return GameMode.CREATIVE;
+        }
+
+        return playerListEntry.getGameMode();
     }
 }
