c4470f4e03c34cc9fad57cb5bac692015711e5ca
Eclipse <eclipse@eclipseisoffline.xyz>
2024-01-19 09:56:33 +0000

    Added `disableKnockback`

--- src/main/java/xyz/eclipseisoffline/eclipsestweakeroo/config/AdditionalDisableConfig.java
+++ src/main/java/xyz/eclipseisoffline/eclipsestweakeroo/config/AdditionalDisableConfig.java
@@ -4,4 +4,5 @@ import fi.dy.masa.malilib.config.options.ConfigBooleanHotkeyed;
 
 public class AdditionalDisableConfig {
     public static final ConfigBooleanHotkeyed DISABLE_ENTITY_COLLISIONS = new ConfigBooleanHotkeyed("disableEntityCollisions", false, "", "Disables entity collisions", "Disable entity collisions");
+    public static final ConfigBooleanHotkeyed DISABLE_KNOCKBACK = new ConfigBooleanHotkeyed("disableKnockback", false, "", "Disables taking knockback", "Disable knockback");
 }
--- src/main/java/xyz/eclipseisoffline/eclipsestweakeroo/mixin/LivingEntityMixin.java
+++ src/main/java/xyz/eclipseisoffline/eclipsestweakeroo/mixin/LivingEntityMixin.java
@@ -1,14 +1,25 @@
 package xyz.eclipseisoffline.eclipsestweakeroo.mixin;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import xyz.eclipseisoffline.eclipsestweakeroo.config.AdditionalDisableConfig;
 import xyz.eclipseisoffline.eclipsestweakeroo.config.AdditionalFeatureToggle;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
+
+    @Inject(method = "takeKnockback", at = @At("HEAD"), cancellable = true)
+    public void takeKnockback(CallbackInfo callbackInfo) {
+        //noinspection ConstantValue
+        if (((Object) this instanceof PlayerEntity) && AdditionalDisableConfig.DISABLE_KNOCKBACK.getBooleanValue()) {
+            callbackInfo.cancel();
+        }
+    }
 
     @Inject(method = "shouldRenderName", at = @At("TAIL"), cancellable = true)
     public void shouldRenderName(CallbackInfoReturnable<Boolean> callbackInfoReturnable) {
