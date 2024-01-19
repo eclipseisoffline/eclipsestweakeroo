80f98736da1c24e788b00e06eadd37bba80d7db8
Eclipse <eclipse@eclipseisoffline.xyz>
2024-01-19 12:55:33 +0000

    Fixed fancy player and mob names showing up in places other than name tags with `tweakPlayerNames` and `tweakMobNames`

--- src/main/java/xyz/eclipseisoffline/eclipsestweakeroo/mixin/EntityMixin.java
+++ src/main/java/xyz/eclipseisoffline/eclipsestweakeroo/mixin/EntityMixin.java
@@ -1,33 +1,15 @@
 package xyz.eclipseisoffline.eclipsestweakeroo.mixin;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.eclipseisoffline.eclipsestweakeroo.config.AdditionalDisableConfig;
-import xyz.eclipseisoffline.eclipsestweakeroo.config.AdditionalFeatureToggle;
-import xyz.eclipseisoffline.eclipsestweakeroo.util.EclipsesTweakerooUtil;
 
 @Mixin(Entity.class)
 public class EntityMixin {
-
-    @Inject(method = "getDisplayName", at = @At("TAIL"), cancellable = true)
-    public void getDisplayName(CallbackInfoReturnable<Text> callbackInfoReturnable) {
-        //noinspection ConstantValue
-        if (AdditionalFeatureToggle.TWEAK_MOB_NAMES.getBooleanValue()
-                && ((Object) this instanceof LivingEntity)) {
-            MutableText name = (MutableText) callbackInfoReturnable.getReturnValue();
-            EclipsesTweakerooUtil.applyFancyName(
-                    (int) ((LivingEntity) (Object) this).getHealth(), name);
-            callbackInfoReturnable.setReturnValue(name);
-        }
-    }
 
     @Inject(method = "Lnet/minecraft/entity/Entity;pushAwayFrom(Lnet/minecraft/entity/Entity;)V", at = @At("HEAD"), cancellable = true)
     public void pushAwayFrom(Entity entity, CallbackInfo callbackInfo) {
---
+++ src/main/java/xyz/eclipseisoffline/eclipsestweakeroo/mixin/EntityRendererMixin.java
@@ -0,0 +1,46 @@
+package xyz.eclipseisoffline.eclipsestweakeroo.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import xyz.eclipseisoffline.eclipsestweakeroo.config.AdditionalFeatureToggle;
+import xyz.eclipseisoffline.eclipsestweakeroo.util.EclipsesTweakerooUtil;
+
+@Mixin(EntityRenderer.class)
+public class EntityRendererMixin {
+
+    @ModifyArg(method = "render",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/entity/EntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V"), index = 1)
+    public Text getDisplayName(Entity entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+        MutableText name = (MutableText) text;
+
+        if (entity instanceof PlayerEntity) {
+            if (AdditionalFeatureToggle.TWEAK_PLAYER_NAME.getBooleanValue()) {
+                if (MinecraftClient.getInstance().getNetworkHandler() == null) {
+                    return name;
+                }
+
+                PlayerListEntry playerListEntry = MinecraftClient.getInstance().getNetworkHandler().getPlayerListEntry(entity.getUuid());
+                EclipsesTweakerooUtil.applyFancyName(playerListEntry,
+                        (int) ((PlayerEntity) entity).getHealth(), name);
+            }
+        } else if (AdditionalFeatureToggle.TWEAK_MOB_NAMES.getBooleanValue()
+                && (entity instanceof LivingEntity)) {
+            EclipsesTweakerooUtil.applyFancyName(
+                    (int) ((LivingEntity) entity).getHealth(), name);
+        }
+
+        return name;
+    }
+}
