76b1386b7811174f2b4826dd24934393a8a8ad86
Eclipse <eclipse@eclipseisoffline.xyz>
2024-01-25 22:26:27 +0000

    Port new features over

--- src/main/java/xyz/eclipseisoffline/eclipsestweakeroo/mixin/InGameHudMixin.java
+++ src/main/java/xyz/eclipseisoffline/eclipsestweakeroo/mixin/InGameHudMixin.java
@@ -2,10 +2,12 @@ package xyz.eclipseisoffline.eclipsestweakeroo.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -24,7 +26,7 @@ public class InGameHudMixin {
     private static final int SPRITE_SIZE = 24;
 
     @Inject(method = "renderStatusEffectOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/effect/StatusEffectInstance;isAmbient()Z"))
-    public void drawStatusText(DrawContext context, CallbackInfo callbackInfo,
+    public void drawStatusText(MatrixStack matrices, CallbackInfo callbackInfo,
             @Local StatusEffectInstance statusEffectInstance, @Local(ordinal = 2) int k, @Local(ordinal = 3) LocalIntRef l) {
         if (AdditionalFeatureToggle.TWEAK_STATUS_EFFECT.getBooleanValue()) {
             TextRenderer renderer = MinecraftClient.getInstance().textRenderer;
@@ -35,8 +37,10 @@ public class InGameHudMixin {
 
             Text durationText = EclipsesTweakerooUtil.getDurationTextWithStyle(statusEffectInstance);
             int textWidth = renderer.getWidth(durationText);
-            context.drawTextWithShadow(renderer, durationText,
-                    k + (SPRITE_SIZE / 2) - (textWidth / 2), l.get() + SPRITE_SIZE + SPACE, -1);
+            renderer.drawWithShadow(matrices, durationText,
+                    k + ((float) SPRITE_SIZE / 2) - ((float) textWidth / 2),
+                    l.get() + SPRITE_SIZE + SPACE, -1);
+            RenderSystem.setShaderTexture(0, HandledScreen.BACKGROUND_TEXTURE);
         }
     }
 }
