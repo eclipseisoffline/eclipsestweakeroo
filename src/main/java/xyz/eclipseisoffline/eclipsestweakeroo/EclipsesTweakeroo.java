46dd5f39e0e15d20091a4f355a21519d5dcadc4b
Eclipse <eclipse@eclipseisoffline.xyz>
2024-02-01 00:19:07 +0000

    Improved durability warnings

--- src/main/java/xyz/eclipseisoffline/eclipsestweakeroo/EclipsesTweakeroo.java
+++ src/main/java/xyz/eclipseisoffline/eclipsestweakeroo/EclipsesTweakeroo.java
@@ -1,20 +1,32 @@
 package xyz.eclipseisoffline.eclipsestweakeroo;
 
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
+import java.util.HashMap;
+import java.util.Map;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
-import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.fabricmc.fabric.api.event.client.player.ClientPreAttackCallback;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import xyz.eclipseisoffline.eclipsestweakeroo.config.AdditionalFeatureToggle;
 import xyz.eclipseisoffline.eclipsestweakeroo.config.AdditionalFixesConfig;
 import xyz.eclipseisoffline.eclipsestweakeroo.config.AdditionalGenericConfig;
 import xyz.eclipseisoffline.eclipsestweakeroo.util.EclipsesTweakerooUtil;
 
 public class EclipsesTweakeroo implements ClientModInitializer {
+    private static final double DURABILITY_WARNING = 0.9;
+    private final Map<EquipmentSlot, Item> registeredItems = new HashMap<>();
+    private final Map<EquipmentSlot, Integer> registeredWarningTimes = new HashMap<>();
 
     @Override
     public void onInitializeClient() {
@@ -24,15 +36,43 @@ public class EclipsesTweakeroo implements ClientModInitializer {
             }
         }));
 
-        AttackBlockCallback.EVENT.register(((player, world, hand, pos, direction) ->
-                useCheck(player, hand) ? ActionResult.PASS : ActionResult.FAIL));
-        AttackEntityCallback.EVENT.register(((player, world, hand, entity, hitResult) ->
-                useCheck(player, hand) ? ActionResult.PASS : ActionResult.FAIL));
-        UseBlockCallback.EVENT.register(((player, world, hand, hitResult) ->
-                useCheck(player, hand) ? ActionResult.PASS : ActionResult.FAIL));
+        ClientPreAttackCallback.EVENT.register(((client, player, clickCount) -> !useCheck(player, Hand.MAIN_HAND)));
+        UseBlockCallback.EVENT.register(((player, world, hand, hitResult) -> useCheck(player, hand)
+                ? ActionResult.PASS : ActionResult.FAIL));
+        UseEntityCallback.EVENT.register(((player, world, hand, entity, hitResult) -> useCheck(player, hand)
+                ? ActionResult.PASS : ActionResult.FAIL));
+        UseItemCallback.EVENT.register(((player, world, hand) -> useCheck(player, hand)
+                ? TypedActionResult.pass(player.getStackInHand(hand)) : TypedActionResult.fail(player.getStackInHand(hand))));
+
+        ClientTickEvents.START_WORLD_TICK.register((world -> {
+            assert MinecraftClient.getInstance().player != null;
+            int time = EclipsesTweakerooUtil.milliTime();
+            for (EquipmentSlot equipmentSlot : EquipmentSlot.values()) {
+                ItemStack itemStack = MinecraftClient.getInstance().player.getEquippedStack(equipmentSlot);
+
+                int requiredDamage = (int) (DURABILITY_WARNING * itemStack.getMaxDamage());
+                if (!itemStack.isDamageable() || itemStack.getDamage() < requiredDamage) {
+                    continue;
+                }
+
+                int warningTime = registeredWarningTimes.getOrDefault(equipmentSlot, 0);
+                boolean check = false;
+                if (!itemStack.getItem().equals(registeredItems.get(equipmentSlot))) {
+                    check = true;
+                } else if ((time - warningTime) / 1000 > AdditionalGenericConfig.DURABILITY_WARNING_COOLDOWN.getIntegerValue()) {
+                    check = true;
+                }
+                if (!check) {
+                    continue;
+                }
+                registeredItems.put(equipmentSlot, itemStack.getItem());
+                registeredWarningTimes.put(equipmentSlot, time);
+                EclipsesTweakerooUtil.showLowDurabilityWarning(itemStack, false);
+            }
+        }));
     }
 
-    private boolean useCheck(PlayerEntity player, Hand hand) {
+    private static boolean useCheck(PlayerEntity player, Hand hand) {
         if (AdditionalGenericConfig.TWEAK_DURABILITY_PREVENT_USE.getBooleanValue()
                 && AdditionalFeatureToggle.TWEAK_DURABILITY_CHECK.getBooleanValue()
                 && player.getStackInHand(hand).isDamageable()) {
